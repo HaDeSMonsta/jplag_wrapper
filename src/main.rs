4043b1844db212d31e13dbc51165a318c1afd8cc
HaDeSMonsta <leon.poellinger+git@gmail.com>
2024-05-08 11:18:41 +0200

    Add foundation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error};
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let target_dir = load_env();
+
+    Ok(())
+}
+
+fn load_env() -> String {
+    dotenv::dotenv()
+        .expect("Unable to finde \".env\" file");
+   
+    let target_dir = env::var("TARGET_DIR")
+        .expect("TARGET_DIR must be set");
+    target_dir
+}
+
+fn prepare_subs() {
+    todo!()
 }
