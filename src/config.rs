cc8f69fafd7c26ceb58364f4b59a1614d83d4df1
HaDeSMonsta <leon.poellinger+git@gmail.com>
2024-11-19 17:42:50 +0100

    Remove previous functions and dependencies, impl config parsing

---
+++ src/config.rs
@@ -0,0 +1,160 @@
+use std::fs;
+use std::fs::OpenOptions;
+use std::io::{BufWriter, Write};
+use std::process::exit;
+use anyhow::{Context, Result};
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+use tracing::{debug, info};
+
+const DEFAULT_CONFIG_FILE: &str = "config.toml";
+const DEFAULT_SOURCE_FILE: &str = "submissions.zip";
+const DEFAULT_TARGET_DIR: &str = "out/";
+const DEFAULT_TMP_DIR: &str = "tmp/";
+
+
+/// TODO Lol
+#[derive(Debug, Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Initialize the config,
+    /// will create or override(!) `config.toml`
+    /// and fill it with the default values
+    #[clap(short, long)]
+    init: bool,
+    /// Specify the config toml file to look for
+    ///
+    /// Will panic, if file does not exist
+    #[clap(short, long)]
+    config: Option<String>,
+    /// Where the input file can be found
+    ///
+    /// Defaults to `submissions.zip`
+    #[clap(short, long)]
+    source: Option<String>,
+    /// Where to put the results
+    ///
+    /// Defaults to `out/`
+    ///
+    /// Warning, this directory will be deleted at application start, if it exists
+    #[clap(short, long)]
+    target_dir: Option<String>,
+    /// Where to put the temporary files
+    ///
+    /// Defaults to `tmp/`
+    ///
+    /// Warning, this directory will be deleted at application start, if it exists
+    #[clap(long)]
+    tmp_dir: Option<String>,
+    _remaining: Vec<String>,
+    #[clap(last = true)]
+    _ignored: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    source: Option<String>,
+    target_dir: Option<String>,
+    tmp_dir: Option<String>,
+}
+
+/// Parse args for the bin, prioritizes cli over toml
+///
+/// Returns: (source, tmp_dir, target_dir)
+pub fn parse_args() -> Result<(String, String, String)> {
+    debug!("Parsing args");
+    let args = Args::parse();
+    if args.init {
+        debug!("Initializing config");
+        dump_default_config()
+            .with_context(|| "Unable to write default config")?;
+    };
+
+    let config = parse_toml(
+        &args.config.unwrap_or_else(|| DEFAULT_CONFIG_FILE.to_string()),
+    ).with_context(|| "Unable to parse toml config")?;
+
+    debug!("Successfully parsed toml");
+
+    let source = args.source
+                     .unwrap_or_else(|| {
+                         config.source
+                               .unwrap_or_else(|| DEFAULT_SOURCE_FILE.to_string())
+                     });
+
+    debug!("Set source to {source}");
+
+    let tmp_dir = args.tmp_dir
+                      .unwrap_or_else(|| {
+                          config.tmp_dir
+                                .unwrap_or_else(|| DEFAULT_TMP_DIR.to_string())
+                      });
+
+    debug!("Set tmp_dir to {tmp_dir}");
+
+    let target_dir = args.target_dir
+                         .unwrap_or_else(|| {
+                             config.target_dir
+                                   .unwrap_or_else(|| DEFAULT_TARGET_DIR.to_string())
+                         });
+
+    debug!("Set target dir to {target_dir}");
+
+    info!("Successfully parsed config");
+
+    Ok((source, tmp_dir, target_dir))
+}
+
+fn parse_toml(file: &str) -> Result<Config> {
+    debug!("Parsing toml, source: {file}");
+    if !fs::exists(&file)
+        .with_context(|| format!("Unable to check if {file} exists"))? {
+        debug!("{file} does not exist");
+        if file != DEFAULT_CONFIG_FILE {
+            panic!("{file} does not exist");
+        }
+        debug!("Returning empty config");
+        return Ok(Config {
+            source: None,
+            target_dir: None,
+            tmp_dir: None,
+        });
+    }
+    let toml = fs::read_to_string(&file)
+        .with_context(|| format!("Failed to read from config file {file}"))?;
+    debug!("Parsing toml, raw: {toml}");
+    Ok(
+        toml::from_str::<Config>(&toml)
+            .with_context(|| format!(
+                "Unable to parse to Config, raw string:\
+            \n\"\"\"\
+            {toml}\
+            \"\"\"\
+            "))?
+    )
+}
+
+fn dump_default_config() -> Result<()> {
+    let conf = Config {
+        source: Some(String::from(DEFAULT_SOURCE_FILE)),
+        target_dir: Some(String::from(DEFAULT_TARGET_DIR)),
+        tmp_dir: Some(String::from(DEFAULT_TMP_DIR)),
+    };
+    debug!("Created default config struct");
+    let file = OpenOptions::new()
+        .create(true)
+        .truncate(true)
+        .write(true)
+        .open(DEFAULT_CONFIG_FILE)
+        .with_context(|| format!("Failed to open/create/truncate config file: {DEFAULT_CONFIG_FILE}"))?;
+    debug!("Opened default config file");
+
+    let mut writer = BufWriter::new(file);
+
+    writeln!(writer, "{}", toml::to_string_pretty(&conf)?)
+        .with_context(|| format!("Unable to write default config to {DEFAULT_CONFIG_FILE}"))?;
+
+    info!("Created default config");
+
+    exit(0);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,194 +1,29 @@
-use std::{env, error, fs, io, thread};
-use std::ffi::OsStr;
-use std::fs::File;
-use std::path::{Path, PathBuf};
-use std::process::Command;
+mod config;
 
-use walkdir::WalkDir;
-use zip::ZipArchive;
+use anyhow::{Context, Result};
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
 
-fn load_env() -> (String, String) {
-    dotenv::dotenv()
-        .expect("Unable to finde \".env\" file");
-
-    let target_dir = env::var("TARGET_DIR")
-        .expect("TARGET_DIR must be set");
-    let temp_dir = env::var("TEMP_DIR").unwrap_or(String::from("temp/"));
-    (target_dir, temp_dir)
-}
-
-fn main() -> Result<(), Box<dyn error::Error>> {
-    let (target_dir, temp_dir) = load_env();
-    rm_tmp_dir(&temp_dir);
-    prepare_subs(&temp_dir)?;
-
-    run(&target_dir)?;
-
-    rm_tmp_dir(&temp_dir);
+fn main() -> Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::DEBUG)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
+    let (source_file, target_dir, temp_dir) = config::parse_args()
+        .with_context(|| "Unable to parse args")?;
 
     Ok(())
 }
 
-fn prepare_subs(temp_dir: &str) -> Result<(), Box<dyn error::Error>> {
-    let root = Path::new("./");
-    let tmp_dir_root = Path::new(temp_dir);
-    generate_root(root, temp_dir)?;
-    unzip_r(&tmp_dir_root)?;
-
-    Ok(())
+fn init() -> Result<()> {
+    todo!()
 }
 
-fn generate_root(dir: &Path, temp_dir: &str) -> Result<(), Box<dyn error::Error>> {
-    let mut zip_file = None;
-
-    for entry in WalkDir::new(dir) {
-        let entry = entry?;
-        if entry.path().extension() == Some(OsStr::new("zip")) {
-            if zip_file.is_some() {
-                return Err(Box::new(io::Error::new(
-                    io::ErrorKind::Other,
-                    "Multiple zip files found!",
-                )));
-            }
-            zip_file = Some(entry.path().to_path_buf());
-        }
-    }
-
-    let zip_file = zip_file.ok_or("No zip files found!")?;
-    fs::create_dir_all(temp_dir)?;
-
-    // TODO remove duplication
-    let file = File::open(&zip_file)?;
-    let mut archive = ZipArchive::new(file)?;
-
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i)?;
-        let out_path = Path::new(temp_dir).join(file.enclosed_name()
-                                                    .expect("Unable to get sanitized name!"));
-
-        if (&*file.name()).ends_with("/") {
-            fs::create_dir_all(&out_path)?;
-            continue;
-        }
-
-        if let Some(p) = out_path.parent() {
-            if !p.exists() { fs::create_dir_all(&p)?; }
-        }
-
-        let mut out_file = File::create(&out_path)?;
-        io::copy(&mut file, &mut out_file)?;
-    }
-
-
-    Ok(())
+fn run() -> Result<()> {
+    todo!()
 }
 
-fn unzip_r(dir: &Path) -> Result<(), Box<dyn error::Error>> {
-    let mut paths = vec![];
-
-    let zip_extension = Some(OsStr::new("zip"));
-
-    for entry in WalkDir::new(&dir) {
-        let entry = entry?;
-        if entry.path().extension() == zip_extension {
-            paths.push(entry.into_path());
-        }
-    }
-
-    while !paths.is_empty() {
-        let new_paths = unzip(
-            paths
-                .pop()
-                .unwrap()
-                .as_path()
-        )?;
-        paths.extend(new_paths);
-    }
-    Ok(())
-}
-
-fn unzip(dir: &Path) -> Result<Vec<PathBuf>, Box<dyn error::Error>> {
-    let mut unzipped_files = vec![];
-    let zip_extension = Some(OsStr::new("zip"));
-
-    // Handle MacOS
-    if let Some(path_str) = dir.to_str() {
-        let target = "MACOSX";
-        if path_str.contains(target) {
-            let macos_idx = path_str.find(target).unwrap();
-            let macos_path_slice = &path_str[..macos_idx + target.len()];
-
-            let macos_path = Path::new(macos_path_slice);
-
-            if !macos_path.exists() { return Ok(vec![]); }
-            if let Err(e) = fs::remove_dir_all(&macos_path) {
-                panic!("Unable to remove MacOS-Path: {macos_path:?} with err {e}\n\
-                Original path was: {dir:?}");
-            }
-            return Ok(vec![]);
-        }
-    }
-
-    let file = File::open(dir)?;
-    let mut archive = match ZipArchive::new(file) {
-        Ok(a) => a,
-        Err(e) => {
-            eprintln!("Unable to extract {dir:?}: {e}");
-            return Ok(vec![]);
-        }
-    };
-    let parent_dir = dir.parent().expect("No parent dir (how?)");
-
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i)?;
-
-        let out_path = file.enclosed_name()
-                           .expect("Unable to get sanitized name!");
-
-        let full_out_path = parent_dir.join(&out_path);
-
-        if (&*file.name()).ends_with("/") {
-            fs::create_dir_all(&full_out_path)?;
-            continue;
-        }
-
-        if let Some(p) = full_out_path.parent() {
-            if !p.exists() { fs::create_dir_all(p)?; }
-        }
-        let mut out_file = File::create(&full_out_path)?;
-        io::copy(&mut file, &mut out_file)?;
-
-        // Add to queue
-        if full_out_path.extension() == zip_extension {
-            // Skip Prog1Tools (and stuff like Prog1Tools (1).zip
-            if let Some(p) = full_out_path.to_str() {
-                if p.to_lowercase().contains("prog1tools") { continue; }
-            }
-            unzipped_files.push(full_out_path);
-        } else if let Some(s) = full_out_path.to_str() {
-            // Make sure to later remove all __MACOSX
-            if s.contains("__MACOSX") { unzipped_files.push(full_out_path) }
-        }
-    }
-
-    fs::remove_file(dir)?;
-
-    Ok(unzipped_files)
-}
-
-fn rm_tmp_dir(temp_dir: &str) {
-    let _ = fs::remove_dir_all(temp_dir);
-}
-
-fn run(target_dir: &str) -> Result<(), Box<dyn error::Error>> {
-    let target_str = String::from(target_dir);
-
-    thread::spawn(move || {
-       Command::new("java")
-           .args(["-jar", ""])
-           .spawn()
-           .unwrap();
-    });
-
-    Ok(())
+fn cleanup() -> Result<()> {
+    todo!()
 }
