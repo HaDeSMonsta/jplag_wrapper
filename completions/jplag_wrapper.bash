5d2f0fa3a3215fbb8c9c8ec825056c100a3469e8
HaDeSMonsta <leon.poellinger+git@gmail.com>
2025-03-10 12:29:11 +0100

    Add completions

---
+++ completions/jplag_wrapper.bash
@@ -0,0 +1,86 @@
+_jplag_wrapper() {
+    local i cur prev opts cmd
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    cmd=""
+    opts=""
+
+    for i in ${COMP_WORDS[@]}
+    do
+        case "${cmd},${i}" in
+            ",$1")
+                cmd="jplag_wrapper"
+                ;;
+            *)
+                ;;
+        esac
+    done
+
+    case "${cmd}" in
+        jplag_wrapper)
+            opts="-d -c -s -t -p -i -j -h -V --init --debug --remove-non-ascii --config --source-zip --target-dir --tmp-dir --preserve-tmp-dir --ignore-file --ignore-output --jplag-jar --help --version [ADD_SUB_DIRS]... [JPLAG_ARGS]..."
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                --config)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -c)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --source-zip)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -s)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --target-dir)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -t)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --tmp-dir)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --ignore-file)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -i)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --jplag-jar)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -j)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+    esac
+}
+
+if [[ "${BASH_VERSINFO[0]}" -eq 4 && "${BASH_VERSINFO[1]}" -ge 4 || "${BASH_VERSINFO[0]}" -gt 4 ]]; then
+    complete -F _jplag_wrapper -o nosort -o bashdefault -o default jplag_wrapper
+else
+    complete -F _jplag_wrapper -o bashdefault -o default jplag_wrapper
+fi
---
+++ completions/jplag_wrapper.elv
@@ -0,0 +1,46 @@
+
+use builtin;
+use str;
+
+set edit:completion:arg-completer[jplag_wrapper] = {|@words|
+    fn spaces {|n|
+        builtin:repeat $n ' ' | str:join ''
+    }
+    fn cand {|text desc|
+        edit:complex-candidate $text &display=$text' '(spaces (- 14 (wcswidth $text)))$desc
+    }
+    var command = 'jplag_wrapper'
+    for word $words[1..-1] {
+        if (str:has-prefix $word '-') {
+            break
+        }
+        set command = $command';'$word
+    }
+    var completions = [
+        &'jplag_wrapper'= {
+            cand -c 'Specify the config toml file to look for if you don''t want to use the default config.toml'
+            cand --config 'Specify the config toml file to look for if you don''t want to use the default config.toml'
+            cand -s 'Where the input file can be found'
+            cand --source-zip 'Where the input file can be found'
+            cand -t 'Where to put the results'
+            cand --target-dir 'Where to put the results'
+            cand --tmp-dir 'Where to put the temporary files'
+            cand -i 'Where to find the ignore file'
+            cand --ignore-file 'Where to find the ignore file'
+            cand -j 'Where the jplag jar can be found'
+            cand --jplag-jar 'Where the jplag jar can be found'
+            cand --init 'Initialize the config, will create (or override!) `config.toml` with all values and fill it with the defaults'
+            cand -d 'Set to use log level `debug`'
+            cand --debug 'Set to use log level `debug`'
+            cand --remove-non-ascii 'Remove all non ASCII characters from all submissions'
+            cand -p 'Set to not remove {{tmp_dir}} when the program finishes'
+            cand --preserve-tmp-dir 'Set to not remove {{tmp_dir}} when the program finishes'
+            cand --ignore-output 'Set to ignore the output of jplag'
+            cand -h 'Print help (see more with ''--help'')'
+            cand --help 'Print help (see more with ''--help'')'
+            cand -V 'Print version'
+            cand --version 'Print version'
+        }
+    ]
+    $completions[$command]
+}
